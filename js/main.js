73baa27b6392bca891086c6cd2863d7e40e7d1e9
Adenike23 <adeofeadenike@gmail.com>
2023-07-28 14:59:19 +0100

    navIcon updated

--- js/main.js
+++ js/main.js
@@ -7,11 +7,24 @@ let typed = new Typed(".typing-effect", {
     loop: true,
   });
 
+  // counting numbers
+  $(".counter").counterUp({
+    delay: 20,
+    time: 2000,
+  });
 
   const userName = JSON.parse(localStorage.getItem('user'));
 if(userName) {
-  document.querySelector('.navRight').style.display = 'none';
-  console.log(document.querySelector('.navRight'));
+  document.querySelector('.navRight').style.display = 'none'
 } else {
   document.querySelector('.ri-user-3-line').style.display = 'none'
 }
+
+const orderButton = document.querySelector('.mainTextButton');
+orderButton.addEventListener('click', function() {
+  if (userName) {
+    location.href = '/dashboard.html'
+  } else {
+    location.href = '/register.html'
+  }
+})
